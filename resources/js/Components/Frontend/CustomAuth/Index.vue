3228504e7282887bb3c29c22ac2da2ca35a1e0c8
Muhibbullah Ansary <muhibbullah611@gmail.com>
2023-01-16 04:40:10 +0600

    working with signin and signup popup

---
+++ resources/js/Components/Frontend/CustomAuth/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-white w-[600px]">
+        <div class="font-semibold flex gap-10 items-center pb-3 mb-3 border-b">
+            <button class="opacity-100">Login</button>
+            <button class="opacity-50">Sign Up</button>
+        </div>
+        <component :is="SignIn" />
+        <button @click="$emit('close')">Close</button>
+    </div>
+</template>
+
+<script setup>
+    import SignIn from '@/Components/Frontend/CustomAuth/SignIn.vue'
+    import SignUp from '@/Components/Frontend/CustomAuth/SignUp.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
