327c2648f28f786a7ec11dc934ed1f0fda66d77d
Muhibbullah Ansary <muhibbullah611@gmail.com>
2023-01-16 11:12:41 +0600

    working with login page create

---
+++ resources/js/Components/Library/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div 
+        @click.self="$emit('update:modelValue', false)"
+        class="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 z-100 grid place-content-center transition-all" 
+        :class="modelValue ? 'opacity-100 visible' : 'opacity-0 invisible'"
+    >
+        <div class="h-[90vh] overflow-y-auto bg-white p-10 shadow">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        modelValue: Boolean
+    })
+    
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/Frontend/Home.vue
+++ resources/js/Pages/Frontend/Home.vue
@@ -3,9 +3,7 @@
         <div class="grid grid-cols-2 h-[100vh] overflow-y-auto">
             <div class="p-4 bg-[#0b021a] relative flex items-center justify-center">
                 <CanvasMenu />
-
                 <Search />
-
             </div>
             <div class="py-10 px-8 bg-[#1c2130] text-white">
                 <div class="flex justify-between items-center">
@@ -14,7 +12,9 @@
                         <h2 class="text-white text-opacity-30 text-xl">For any occasion, from your preferred personality</h2>
                     </div>
                     <div>
-                        <button class="uppercase font-black px-6 py-2 border border-white border-opacity-50 text-white text-opacity-60 hover:text-opacity-100 rounded-full">Sign Up</button>
+                        <button @click="showModal = true" class="font-black px-6 py-2 border border-white border-opacity-50 text-white text-opacity-60 hover:text-opacity-100 rounded-full">
+                            Sign Up / Login
+                        </button>
                     </div>
                 </div>
 
@@ -28,15 +28,24 @@
                 <Footer class="mt-20"/>
             </div>
         </div>
+
+        <Modal v-model="showModal">
+            <CustomAuth @close="showModal = false" />
+        </Modal>
     </Master>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Master from '@/Components/Frontend/Master.vue';
 import CategoryWiseItem from '@/Components/Frontend/CategoryWiseItem.vue'
 import Search from '@/Components/Frontend/Home/Search.vue'
 import CanvasMenu from '@/Components/Frontend/Home/CanvasMenu.vue'
+import Modal from '@/Components/Library/Modal.vue'
+import CustomAuth from '@/Components/Frontend/CustomAuth/Index.vue'
 import Footer from '@/Components/Backend/Global/Footer.vue'
+
+const showModal = ref(false)
 </script>
 
 <style lang="scss" scoped>
