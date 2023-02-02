94e4aea829a44b60de3041fee8b5f5e75939c98c
Muhibbullah Ansary <muhibbullah611@gmail.com>
2023-02-03 04:26:52 +0600

    chart implemented, in Talent Dashboard

---
+++ resources/js/Components/Backend/TalentDashboard/Analytics/Widget.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="grid grid-cols-5 gap-6">
+        <div class="border shadow-lg rounded overflow-hidden">
+            <div class="p-4 py-2 bg-sky-400 font-bold text-white text-center">Wish (2)</div>
+            <div class="p-4 py-2 items-center justify-center font-bold flex gap-1">
+                $90 <span class="text-[15px] text-sky-500">(0)</span>
+            </div>
+        </div>
+        <div class="border shadow-lg rounded overflow-hidden">
+            <div class="p-4 py-2 bg-orange-300 font-bold text-white text-center">Tips (5)</div>
+            <div class="p-4 py-2 items-center justify-center font-bold flex gap-1">
+                $50 <span class="text-[15px] text-orange-400">(0)</span>
+            </div>
+        </div>
+        <div class="border shadow-lg rounded overflow-hidden">
+            <div class="p-4 py-2 bg-green-400 font-bold text-white text-center">My Life (2)</div>
+            <div class="p-4 py-2 items-center justify-center font-bold flex gap-1">
+                $20 <span class="text-[15px] text-green-400">(2)</span>
+            </div>
+        </div>
+        <div class="border shadow-lg rounded overflow-hidden">
+            <div class="p-4 py-2 bg-blue-400 font-bold text-white text-center">Calendar (0)</div>
+            <div class="p-4 items-center justify-center font-bold flex gap-1">
+                $0 <span class="text-[15px] text-green-400">(0)</span>
+            </div>
+        </div>
+        <div class="border shadow-lg rounded overflow-hidden">
+            <div class="p-4 py-2 bg-sky-400 font-bold text-white text-center">Revenue Gross (0)</div>
+            <div class="p-4 items-center justify-center font-bold flex gap-1">
+                $0 <span class="text-[15px] text-green-400">(0)</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/Pages/Backend/TalentDashboard/Analytics.vue
+++ resources/js/Pages/Backend/TalentDashboard/Analytics.vue
@@ -4,8 +4,19 @@
             <LeftSide />
         </template>
         <template v-slot:content>
+            <div class="flex gap-6 border rounded p-4 mb-2">
+                <div class="w-full">
+                    <h1 class="text-sm font-bold">Start Date</h1>
+                    <CInput type="date" />
+                </div>
+                <div class="w-full">
+                    <h1 class="text-sm font-bold">End Date</h1>
+                    <CInput type="date" />
+                </div>
+            </div>
+            <Widget />
             <VueApexCharts
-                width="500"
+                height="500"
                 type="bar"
                 :options="chartOptions"
                 :series="series"
@@ -15,24 +26,26 @@
 </template>
 
 <script setup>
-    import DashboardLayout from '../DashboardLayout.vue'
-    import LeftSide from '@/Components/Backend/TalentDashboard/LeftSide.vue'
-    import VueApexCharts from "vue3-apexcharts"
+import DashboardLayout from '../DashboardLayout.vue'
+import LeftSide from '@/Components/Backend/TalentDashboard/LeftSide.vue'
+import VueApexCharts from "vue3-apexcharts"
+import Widget from '@/Components/Backend/TalentDashboard/Analytics/Widget.vue';
+import CInput from '@/Components/Global/CInput.vue';
 
-    const chartOptions = {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-        },
-    }
-    const series = [
-        {
-          name: "series-1",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
-        },
-    ]
+const chartOptions = {
+    chart: {
+        id: "vuechart-example",
+    },
+    xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+}
+const series = [
+    {
+        name: "series-1",
+        data: [30, 40, 35, 50, 49, 60, 70, 91],
+    },
+]
 </script>
 
 
